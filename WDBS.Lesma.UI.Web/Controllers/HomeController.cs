00f92a06ee25d475fdc0a2b7ff316e946209e1a6
franciscomazali <franciscomazali@gmail.com>
2014-03-21 01:19:24 -0300

    envia

--- WDBS.Lesma.UI.Web/Controllers/HomeController.cs
+++ WDBS.Lesma.UI.Web/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -16,5 +17,76 @@ namespace WDBS.Lesma.UI.Web.Controllers
             return View();
         }
 
+        [HttpPost]
+        public ActionResult EnviaEmail(string email, string nome, string mensagem)
+        {
+            //Response.Cache.SetCacheability(HttpCacheability.NoCache);
+            //Response.Cache.SetAllowResponseInBrowserHistory(false);
+
+            //string email = form["email"];
+            //string nome = form["nome"];
+            //string mensagem = form["mensagem"];
+
+
+
+            if (Helper.Email.EnviaEmail(nome, "Contato do site", MontaMensagemEmail(nome, email, mensagem), "franciscomazali@gmail.com"))
+                TempData.Add("Sucesso", "Mensagem enviada com sucesso.");
+
+            else
+                TempData.Add("Erro", "Ops. Ocorreu um erro em nosso servidor. Por favor, tente outra vez mais tarde.");
+
+            return View();
+        }
+
+
+        private string MontaMensagemEmail(string nome, string email, string mensagem)
+        {
+            StringBuilder body = new StringBuilder();
+
+            body.AppendLine("<table>");
+            body.AppendLine("<tr><td>Nome: </td><td>" + nome + "</td></tr>");
+            body.AppendLine("<tr><td>E-mail: </td><td>" + email + "</td></tr>");
+            body.AppendLine("<tr><td>Mensagem: </td><td>" + mensagem + "</td></tr>");
+            body.AppendLine("<tr><td><br /></td></tr>");
+            body.AppendLine("</table>");
+            body.AppendLine("<p>**Este e-mail não deve ser respondido. Por favor, entre contato com o remetente desta mensagem em: " + email + " **</p>");
+
+            return body.ToString();
+
+        }
+
+        [HttpPost]
+        public ActionResult EnviaEmailEcomenda(string encomenda)
+        {
+            //Response.Cache.SetCacheability(HttpCacheability.NoCache);
+            //Response.Cache.SetAllowResponseInBrowserHistory(false);
+
+            //string email = form["encomenda"];
+
+
+            if (Helper.Email.EnviaEmail(encomenda, "Você tem uma nova encomenda!", MontaMensagemEncomenda(email), "franciscomazali@gmail.com"))
+                TempData.Add("Sucesso", "Encomenda realizada com sucesso.");
+
+            else
+                TempData.Add("Erro", "Ops. Ocorreu um erro em nosso servidor. Por favor, tente outra vez mais tarde.");
+
+            return View();
+        }
+
+
+        private string MontaMensagemEncomenda(string email)
+        {
+            StringBuilder body = new StringBuilder();
+
+            body.AppendLine("<h2>Você tem uma encomenda!</h2>");
+            body.AppendLine("<table>");
+            body.AppendLine("<tr><td>O remetente " + email + " gostaria de receber mais noticias sobre o livro. Por favor, entre em contato!</td></tr>");
+            body.AppendLine("<tr><td><br /></td></tr>");
+            body.AppendLine("</table>");
+            body.AppendLine("<p>**Este e-mail não deve ser respondido.**</p>");
+
+            return body.ToString();
+
+        }
     }
 }
---
+++ WDBS.Lesma.UI.Web/Helper/Email.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+
+namespace WDBS.Lesma.UI.Web.Helper
+{
+    public class Email
+    {
+        public static bool EnviaEmail(string nome, string assunto, string mensagem, string destinatario = "lesma.selma@gmail.com")
+        {
+            //Define os dados do e-mail
+            string nomeRemetente = "Contato Lesma";
+            string emailRemetente = "lesma@wdbs.com.br";
+            string senha = "selma01";
+
+            //Host da porta SMTP
+            string SMTP = "smtp.wdbs.com.br";
+
+            string emailDestinatario = destinatario;
+
+            //Cria objeto com dados do e-mail.
+            MailMessage objEmail = new MailMessage();
+
+            //Define o Campo From e ReplyTo do e-mail.
+            objEmail.From = new System.Net.Mail.MailAddress(emailRemetente, nome + " <" + emailRemetente + ">");
+
+            //Define os destinatários do e-mail.
+            objEmail.To.Add(emailDestinatario);
+
+            //Define a prioridade do e-mail.
+            objEmail.Priority = System.Net.Mail.MailPriority.Normal;
+
+            //Define o formato do e-mail HTML (caso não queira HTML alocar valor false)
+            objEmail.IsBodyHtml = true;
+
+            //Define título do e-mail.
+            objEmail.Subject = assunto;
+
+            //Define o corpo do e-mail.
+            objEmail.Body = mensagem;
+
+            //Para evitar problemas de caracteres "estranhos", configuramos o charset para "ISO-8859-1"
+            objEmail.SubjectEncoding = System.Text.Encoding.GetEncoding("ISO-8859-1");
+            objEmail.BodyEncoding = System.Text.Encoding.GetEncoding("ISO-8859-1");
+
+            System.Net.Mail.SmtpClient objSmtp = new System.Net.Mail.SmtpClient();
+
+            //Alocamos o endereço do host para enviar os e-mails  
+            objSmtp.Credentials = new System.Net.NetworkCredential(emailRemetente, senha);
+            objSmtp.Host = SMTP;
+            objSmtp.Port = 587;
+            objSmtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+
+            try
+            {
+                objSmtp.Send(objEmail);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+            finally
+            {
+                //excluímos o objeto de e-mail da memória
+                objEmail.Dispose();
+                //anexo.Dispose();
+            }
+
+        }
+    }
+}
