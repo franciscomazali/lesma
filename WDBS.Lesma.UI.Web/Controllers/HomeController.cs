37baed5c38b1b1ab316a9adbfefb4b26e746903d
francisco <franciscomazali@gmail.com>
2014-04-09 15:48:58 -0300

    ca

--- WDBS.Lesma.UI.Web/Controllers/HomeController.cs
+++ WDBS.Lesma.UI.Web/Controllers/HomeController.cs
@@ -64,7 +64,7 @@ namespace WDBS.Lesma.UI.Web.Controllers
             //string email = form["encomenda"];
             string msg = "";
 
-            if (Helper.Email.EnviaEmail(encomenda, "Você tem uma nova encomenda!", MontaMensagemEncomenda(encomenda), "franciscomazali@gmail.com"))
+            if (Helper.Email.EnviaEmail(encomenda, "Você tem uma nova encomenda!", MontaMensagemEncomenda(encomenda)))
                 msg = "Encomenda realizada com sucesso.";
 
             else
